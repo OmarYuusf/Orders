566d166ad1a1c0aa8b258027dbaae87fcfc39909
OmarYuusf <57051225+OmarYuusf@users.noreply.github.com>
2020-03-04 02:13:01 -0800

    Updates

---
+++ precache-manifest.b0ba68fc3feec860db601b848a60831d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4c7d02bdb5c886ba86d38ebe0f1c9af",
+    "url": "/Orders/index.html"
+  },
+  {
+    "revision": "d1ccd02384b538c33328",
+    "url": "/Orders/static/css/main.2727f306.chunk.css"
+  },
+  {
+    "revision": "1c9c7f21041a1a5ace89",
+    "url": "/Orders/static/js/2.974b264e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Orders/static/js/2.974b264e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1ccd02384b538c33328",
+    "url": "/Orders/static/js/main.80bdde17.chunk.js"
+  },
+  {
+    "revision": "58515b57f140bcb29e07",
+    "url": "/Orders/static/js/runtime-main.6692adc9.js"
+  }
+]);
